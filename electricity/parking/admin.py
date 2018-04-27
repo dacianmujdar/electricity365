591249287c900d9371e0363bbf4f04050e8fffe2
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-04-28 00:54:27 +0300

    update imports

--- electricity/parking/admin.py
+++ electricity/parking/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from models import CameraInput, ParkingSpot, CameraParkingSpot
+from electricity.parking.models import CameraInput, ParkingSpot, CameraParkingSpot
 
 admin.register(CameraInput)
 admin.register(ParkingSpot)
