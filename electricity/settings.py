e02ecf506d39b0bda878ecbec4069ed77aa756a1
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-04-28 01:39:25 +0300

    added st

--- electricity/settings.py
+++ electricity/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'electricity/static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
