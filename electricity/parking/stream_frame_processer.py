6c0b6c36de2a82eff7b7112bebc468f8d145b6b0
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-04-29 17:48:37 +0300

    added opencv3

--- electricity/parking/stream_frame_processer.py
+++ electricity/parking/stream_frame_processer.py
@@ -1,6 +1,6 @@
 from electricity.parking.models import CameraParkingSpot, CameraInput
 import numpy as np
-import cv2
+import cv3
 from PIL import Image
 
 from electricity.predictor.predictor import Predictor
@@ -10,11 +10,11 @@ class StreamFrameProcesser:
 
     @staticmethod
     def refresh_frames():
-        video = cv2.VideoCapture()
+        video = cv3.VideoCapture()
         for camera in CameraInput.objects.all():
             #url = camera.url
             url = 'https://video-weaver.fra02.hls.ttvnw.net/v1/playlist/CoMD2YW4YVAR8uKgwZJ-g1SnEj30geTJyIjZjKWAKbdJtI6t8LI4NE4h_VpfKgvZ9zhrgnPyHgR6ktuqVCOYn_B4kWaq-cgvZXts-39-cK2QyyRKB9VxTGj4okfQ4Xv_M60SofZzTLd_zuzU87u8WL6WXDDjqo6Z7F2__4pXu0hMCyFeC5a33F1jITUJ8cFzeoPg0CKRK2Kbk4Rh8DSY2YjxPPPt8DAywiN5bMDBpEpvLdqdL76lbdMuV_pa1DV56t-25kp3SoAGuUjPaVb6cdBRX3z2rUzip_NoRyX2WZIOC9n52wPNbwh9hUI7_F59fjnkOaWexEQAOgToE19YIE9AfC_V04GQAMS1thyOdVNeD4PzidqueROXzdiVX3wZYTgFisr3kypqkh0nceMeRrSoWemYtnSwbRShzBs51yZPVcJKhYexvcNW4uC88Z2iZDVyroXYJPgtFOCaAu6NuuMM-kBHVzFzpWZEpK3Bfqn2-5sv_I76mTp_gX-64M09hmZRH-XiEhCUzygeNns0I8dlbYACCMWvGgye57gwe1RXPTFomfA.m3u8'
-            video = cv2.VideoCapture()
+            video = cv3.VideoCapture()
             video.open(url)
             success, image = video.read()
             if success:
