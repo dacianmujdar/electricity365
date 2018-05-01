4903342c76b927e151ccd278b51cf3ddf99fc730
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-05-01 19:09:20 +0300

    ads

--- electricity/parking/stream_frame_processer.py
+++ electricity/parking/stream_frame_processer.py
@@ -13,7 +13,7 @@ class StreamFrameProcesser:
     def refresh_frames():
         for camera in CameraInput.objects.all():
             video = cv2.VideoCapture()
-            url = "https://video-weaver.fra02.hls.ttvnw.net/v1/playlist/CoMDcSbBLQvLi-jB93IEJuacqAPadhDoXFdDEpSJfzFpx9iNchykWSrIaVVGnnTqyojZOegr3AbHJAkFxPjPtnSP8y3dN3jNPrwHVzLKALSRf1X6POzYcAYMdOAbjwjOeDqR3KawFcEkq6xHWXguyfupOOFJcszrymr0Pect-3Yjv2vt1mAugQdcHNY3r2VlzBcYwyLmPT6kj9QnKVtmlT20sAJJIMuGeCf_-oD9Tk-4hWDOG9JSvPQr3g-iitFdAp_40aJcTWT1rw6MQwOUq_K_mU_SkW7DtjunQ7lYPutHI0QNR0tK0qBtZJjfPPNGhRT2afbRq3nR6ncp1qQgpK8KENi0huRJOP5fzaCDJyDC9g2Pe4fZdIbiaOu4F_genlmr_WYxmvd94E_DSWJ5aaeEIy-GDN3uRJWZoe667PormmKpi40i0WO3sdodvyBpaxf7RZ7G3siObaHt_6Ig5paAdNZBNE7vJ0dwF39g9v_IppUwT-W_4mVEv-EHUS4TqYLcf08mEhCpCnOCa_uDaaXKKsIh7hWJGgwbqvUilAu2VF5QCrk.m3u8"
+            url = 'https://hddn01.skylinewebcams.com/live.m3u8?a=5tm6kfqrhqbpblan9j5d4bmua4'
             video.open(url)
             success, image = video.read()
             Predictor.predict(image_path='t.png')
