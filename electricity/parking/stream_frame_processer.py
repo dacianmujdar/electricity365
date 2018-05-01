0d0550f31f1509fc99d29c69f480d7a3551fe36a
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-05-01 11:54:23 +0300

    dacian

--- electricity/parking/stream_frame_processer.py
+++ electricity/parking/stream_frame_processer.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 from PIL import Image
 import logging
-import skvideo.io
+
 from electricity.predictor.predictor import Predictor
 
 
@@ -12,9 +12,9 @@ class StreamFrameProcesser:
     @staticmethod
     def refresh_frames():
 
-        video = skvideo.io.VideoCapture()
+        video = cv2.VideoCapture()
         for camera in CameraInput.objects.all():
-            video = skvideo.io.VideoCapture()
+            video = cv2.VideoCapture()
             url = "https://video-weaver.cdg02.hls.ttvnw.net/v1/playlist/CoMD69PS9fTrFfErZIkqqJTfZ_SKxP4R1CzsRzAG1rMNXies_LNjpFDGml-J-ULdd0bjFYF98sU3AnG_yYYzyXon25u_p7M0lL4t1olN8IkpdYnwJr-OVGVa3aT-qz5V8yeoblEQWK32Jj0sv6e9WpAqFAG-Ou4vIy2fkXh0BQXNJmRfxvRcdimSWTFbPdupeByqGZu9RwCv86ek5Q0t2-FtHKQY1Me-LMKQJHDH56aoAk1_whtEaquFNH-JLTMITFEGgU2x9wpdsXuU7j4hqnQeIsPM1JbQ-dwSjRmBQilLy6v_FX-Q9qatKdRfeFyWd-H81JIe6ElyioPr0UvgBRmKS86op2p03YgWd-kzD7niv8tZDY8JdzYJ7SEPyKOILWbxHbA9B2V4qAoH_0lVX-9C3eSQqDRax4KbPNhMmi44QFGS_ApPtH1yINGR8MzKwYRWzmjJw9pq_GuCs8qu7mCBh7V-xBh_mKfRq1C-ZArcKJPnxZls9W0MyFTDS31kvy6G7P7dEhB9oePJmNzFZClo0MXfpCNyGgypAqoU-d2hU3nBang.m3u8"
             video.open(url)
             success, image = video.read()
