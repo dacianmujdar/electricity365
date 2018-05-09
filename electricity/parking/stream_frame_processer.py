83e0f770dc054765b3adfd90ea0dbe94119b52ab
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-05-09 11:21:07 +0300

    ads

--- electricity/parking/stream_frame_processer.py
+++ electricity/parking/stream_frame_processer.py
@@ -13,9 +13,10 @@ class StreamFrameProcesser:
     def refresh_frames():
         for camera in CameraInput.objects.all():
             video = cv2.VideoCapture()
-            url = 'http://content.jwplatform.com/manifests/vM7nH0Kl.m3u8'
+            url = 'https://hddn01.skylinewebcams.com/live.m3u8?a=5tm6kfqrhqbpblan9j5d4bmua4'
             video.open(url)
             success, image = video.read()
+            Predictor.predict(image_path='t.png')
             if success:
                 for camera_parking_spot in camera.parking_spots.all():
                     upper_left = [camera_parking_spot.upper_right_x, camera_parking_spot.upper_right_y]
