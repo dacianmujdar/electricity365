339ab9fa769c2bb94e0ad7768bd2bfc97053db28
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-05-15 09:55:02 +0300

    added filtering

--- electricity/parking/views.py
+++ electricity/parking/views.py
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import math
+
 from rest_framework import generics
 
 from electricity.parking.models import ParkingSpot
 from electricity.parking.serializers import ParkingSpotSerializer
 
+r_earth = 6371000.0
+
 
 def nearby_spots(location_latitude, location_longitude, radius):
-    return ParkingSpot.objects.all()
+
+    delta_lat = (radius / r_earth) * (180 / math.pi)
+    delta_lon = (radius / r_earth) * (180 / math.pi) / math.cos(delta_lat * math.pi / 180)
+
+    max_lat = location_latitude + delta_lat
+    min_lat = location_latitude - delta_lat
+
+    max_lon = location_longitude + delta_lon
+    min_lon = location_longitude - delta_lon
+
+    return ParkingSpot.objects.filter(latitude_gte=max_lat, latitude__lte=min_lat,
+                                      longitude_gte=max_lon, longitude_lte=min_lon)
 
 
 class ParkingSpotList(generics.ListAPIView):
