1bf8a9be8ae45c881124c242a5f65ed928ab4fdf
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-05-15 10:05:51 +0300

    added bounding box v2

--- electricity/parking/views.py
+++ electricity/parking/views.py
@@ -8,22 +8,30 @@ from rest_framework import generics
 from electricity.parking.models import ParkingSpot
 from electricity.parking.serializers import ParkingSpotSerializer
 
-r_earth = 6371000.0
+
+def get_bounding_box(latitude_in_degrees, longitude_in_degrees, half_side_in_meters):
+    half_side_in_km = half_side_in_meters / 1000
+    lat = math.radians(latitude_in_degrees)
+    lon = math.radians(longitude_in_degrees)
+
+    radius = 6371
+    # Radius of the parallel at given latitude
+    parallel_radius = radius*math.cos(lat)
+
+    lat_min = lat - half_side_in_km/radius
+    lat_max = lat + half_side_in_km/radius
+    lon_min = lon - half_side_in_km/parallel_radius
+    lon_max = lon + half_side_in_km/parallel_radius
+    rad2deg = math.degrees
+
+    return rad2deg(lat_max), rad2deg(lat_min), rad2deg(lon_max), rad2deg(lon_min)
 
 
 def nearby_spots(location_latitude, location_longitude, radius):
 
-    delta_lat = (radius / r_earth) * (180 / math.pi)
-    delta_lon = (radius / r_earth) * (180 / math.pi) / math.cos(delta_lat * math.pi / 180)
-
-    max_lat = location_latitude + delta_lat
-    min_lat = location_latitude - delta_lat
-
-    max_lon = location_longitude + delta_lon
-    min_lon = location_longitude - delta_lon
-
-    return ParkingSpot.objects.filter(latitude_gte=max_lat, latitude__lte=min_lat,
-                                      longitude_gte=max_lon, longitude_lte=min_lon)
+    bounding_box = get_bounding_box(location_latitude, location_longitude, radius)
+    return ParkingSpot.objects.filter(latitude_gte=bounding_box[0], latitude__lte=bounding_box[1],
+                                      longitude_gte=bounding_box[2], longitude_lte=bounding_box[3])
 
 
 class ParkingSpotList(generics.ListAPIView):
