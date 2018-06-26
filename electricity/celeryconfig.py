566fe2c75a5d446250fa94ef1885136733c4e6ea
Dacian Mujdar <dacian.mujdar@rodeapps.com>
2018-06-26 16:43:34 +0300

    updates

--- electricity/celeryconfig.py
+++ electricity/celeryconfig.py
@@ -3,11 +3,11 @@
 try:
 	from local_celeryconfig import *
 except ImportError:
-	BROKER_URL = 'amqp://ohyqqudn:xagxV0lNAGzaDtO3VGOgpqmFyU8lbwxZ@wolverine.rmq.cloudamqp.com/ohyqqudn'
+	BROKER_URL = 'amqp://bdqnhtsp:okrRIv5DSiHRJ7mRTULvKCimsCz4CC9B@baboon.rmq.cloudamqp.com/bdqnhtsp'
 	BROKER_POOL_LIMIT = 1
 	BROKER_CONNECTION_TIMEOUT = 10
 	CELERYD_CONCURRENCY = 4
-	CELERY_RESULT_BACKEND = 'amqp://ohyqqudn:xagxV0lNAGzaDtO3VGOgpqmFyU8lbwxZ@wolverine.rmq.cloudamqp.com/ohyqqudn'
+	CELERY_RESULT_BACKEND = 'amqp://bdqnhtsp:okrRIv5DSiHRJ7mRTULvKCimsCz4CC9B@baboon.rmq.cloudamqp.com/bdqnhtsp'
 
 	CELERY_TASK_SERIALIZER = 'json'
 	CELERY_RESULT_SERIALIZER = 'json'
--- electricity/settings.py
+++ electricity/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'electricity.parking',
+    'electricity.predictor',
     'background_task',
     'corsheaders',
     'django_extensions',
@@ -78,17 +79,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'electricity.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
